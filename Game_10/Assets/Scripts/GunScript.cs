f99cf00234e60321aeee1f9ff816978703aaa5db
Komine3000 <dj18488@gmail.com>
2022-11-27 17:15:44 -0800

    v1.0
    I added the game

---
+++ Game_10/Assets/Scripts/GunScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    Vector3 screenPos;
+    Vector3 worldPos;
+    Vector3 mousePos;
+    Vector2 mousePos2D;
+    Vector3 dir;
+    float angle;
+    
+    void Update()
+    {
+        screenPos = Input.mousePosition;
+        screenPos.z = Camera.main.nearClipPlane + 1;
+        worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+        mousePos = new Vector3((Mathf.Round(worldPos.x)), (Mathf.Round(worldPos.y)), 0);
+        
+        if (dir != null)
+        {
+            dir = mousePos - this.transform.position;
+            angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        }
+    }
+}
