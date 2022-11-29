5a755e36931234fabe1eb62970bb6fe00a605452
NinjaCad <106050883+NinjaCad@users.noreply.github.com>
2022-11-28 20:59:52 -0800

    v1.1.0
    Added wall slide and wall jump

--- Game_10/Assets/Scripts/PlayerScript.cs
+++ Game_10/Assets/Scripts/PlayerScript.cs
@@ -29,28 +29,23 @@ public class PlayerScript : MonoBehaviour
     int currentAmmo;
     [SerializeField] int maxAmmo;
 
+    [SerializeField] float wallSlidingSpeed;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         //gMS = gameManager.GetComponent<GameManagerScript>();
     }
-    
-    bool isGrounded()
-    {
-        raycast = Physics2D.BoxCast(new Vector3(transform.position.x, transform.position.y - 0.55f), new Vector3(1, 0.1f, 1), 0f, Vector2.down, 0f, collisionLayers);
-        return raycast.collider != null;
-    }
-    
+
     void Update()
     {
         screenPos = Input.mousePosition;
         screenPos.z = Camera.main.nearClipPlane + 1;
         worldPos = Camera.main.ScreenToWorldPoint(screenPos);
         mousePos = new Vector3((Mathf.Round(worldPos.x)), (Mathf.Round(worldPos.y)), 0);
-        
-        reloadTime -= Time.deltaTime;
-        
+
+        velX = Input.GetAxisRaw("Horizontal");
+
         if (isGrounded())
         {
             coyote = 0.10f;
@@ -72,11 +67,10 @@ public class PlayerScript : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
             buffer = 0f;
+            reloadTime = 0f;
         }
-        
-        velX = Input.GetAxisRaw("Horizontal") * speedX;
 
-        if(Input.GetMouseButtonDown(0) && currentAmmo > 0 && reloadTime < 0f)
+        if(Input.GetMouseButtonDown(0) && currentAmmo > 0 && reloadTime <= 0f)
         {
             playerToMouse = (mousePos - transform.position) * -1;
             angle = Mathf.Atan2(playerToMouse.y, playerToMouse.x);
@@ -85,13 +79,47 @@ public class PlayerScript : MonoBehaviour
             {
                 rb.velocity = new Vector2(rb.velocity.x, 25f);
             }
-            reloadTime = 0.4f;
+            reloadTime = 0.3f;
             currentAmmo -= 1;
+        } else
+        {
+            reloadTime -= Time.deltaTime;
         }
+
+        if (isWalled() == true && isGrounded() == false && velX != 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = new Vector2(-15f * velX, jumpSpeed);
+            }
+        }
+
+        if (velX > 0)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+        if (velX < 0)
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+        }
+    }
+
+    bool isGrounded()
+    {
+        raycast = Physics2D.BoxCast(new Vector2(transform.position.x, transform.position.y - 0.505f), new Vector3(1f, 0.01f, 1f), 0f, Vector2.down, 0f, collisionLayers);
+        return raycast.collider != null;
+    }
+
+    bool isWalled()
+    {
+        raycast = Physics2D.BoxCast(new Vector2(transform.position.x + (0.505f * velX > 0 ? 1 : -1), transform.position.y), new Vector3(0.01f, 1f, 1f), 0f, Vector2.down, 0f, collisionLayers);
+        return raycast.collider != null;
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, velX, resistance), rb.velocity.y);
+        rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, velX * speedX, resistance), rb.velocity.y);
     }
 }
